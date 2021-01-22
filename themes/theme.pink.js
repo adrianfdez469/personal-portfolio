33ae8f9adb7c7854f70c11b3d5514a487a0302b7
Adrian <adrianfdez469@gmail.com>
2021-01-22 16:55:14 -0500

    Adding themes

---
+++ themes/theme.pink.js
@@ -0,0 +1,22 @@
+import { createMuiTheme } from '@material-ui/core';
+// import { red } from '@material-ui/core/colors';
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ad1457',
+    },
+    secondary: {
+      main: '#f06292',
+    },
+    error: {
+      // main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
+
+export default theme;
--- themes/theme.purple.js
+++ themes/theme.purple.js
@@ -1,14 +1,14 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core';
 import { red } from '@material-ui/core/colors';
 
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#7e57c2',
     },
     secondary: {
-      main: '#19857b',
+      main: '#7986cb',
     },
     error: {
       main: red.A400,
