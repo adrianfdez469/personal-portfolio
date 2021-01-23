39353d5b71537252b15f7d27760a7dde15cb050c
Adrian <adrianfdez469@gmail.com>
2021-01-23 17:38:39 -0500

    Improving performance

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import { signIn, signOut, useSession } from 'next-auth/client';
+import { signOut, useSession } from 'next-auth/client';
+import Link from 'next/link';
 
 export default function myComponent() {
   const [session /* , loading */] = useSession();
+
   return (
     <>
       {!session && (
         <>
           Not signed in
           <br />
-          <button onClick={signIn} type="button">
+          <Link href="/auth/signin?callbackUrl=http://localhost:3000/">
+            <a>Signin</a>
+          </Link>
+          {/* <button onClick={signIn} type="button">
             Sign in
-          </button>
+          </button> */}
         </>
       )}
       {session && (
