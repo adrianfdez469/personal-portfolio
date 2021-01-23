64cd9e6172a7756131b20881ee993926b429cf5d
Adrian <adrianfdez469@gmail.com>
2021-01-22 20:04:56 -0500

    Removing default generated code from 'create next-app cli

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,29 @@
+import React from 'react';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
-export default function Home() {
+export default function myComponent() {
+  const [session /* , loading */] = useSession();
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <>
+      {!session && (
+        <>
+          Not signed in
+          <br />
+          <button onClick={signIn} type="button">
+            Sign in
+          </button>
+        </>
+      )}
+      {session && (
+        <>
+          Signed in as
+          {`${session.user.name} ${session.user.email}`}
+          <br />
+          <button onClick={signOut} type="button">
+            Sign out
+          </button>
+        </>
+      )}
+    </>
   );
 }
