a2adb5e152f6423215f961db47cd7f9b26c8159f
Jorge Arce <jarce@xetid.cu>
2021-01-27 22:33:54 +0100

    Actualizando vista estática para el componente Profile.

--- components/UI/Profile/index.js
+++ components/UI/Profile/index.js
@@ -1,4 +1,3 @@
-export { default as TopBar } from './TopBar';
 export { default as PersonData } from './PersonData';
 export { default as AddProject } from './AddProject';
 export { default as ProjectBox } from './ProjectBox';
