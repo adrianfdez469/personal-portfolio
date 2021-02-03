8471e59c9b572546be1debf7e46dd5f3d0125c60
Jose Adrian Iglesias Ruiz <joseadrian1007@gmail.com>
2021-02-02 21:02:43 -0500

    fixing card button collor issue when dark theme is active

--- components/UI/cards/styles.js
+++ components/UI/cards/styles.js
@@ -47,7 +47,7 @@ export default makeStyles((theme) => ({
     width: '90%',
   },
   cardFrontBarButton: {
-    backgroundColor: '#ffffff',
+    backgroundColor: theme.palette.background.default,
     marginRight: '0.5em',
     marginLeftt: '0.5em',
     '&:hover': {
