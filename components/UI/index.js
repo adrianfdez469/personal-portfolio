d90140c65d94eacb25ea24a6a1a85d9b9f1cfaea
Jorge Arce <jarce@xetid.cu>
2021-02-04 01:22:18 +0100

    Eliminando archivos obsoletos.

--- components/UI/index.js
+++ components/UI/index.js
@@ -4,3 +4,5 @@ export { default as LandingFeatures } from './landing/LandingFeatures';
 export { default as LandingFooter } from './landing/LandingFooter';
 export { default as LandingIntro } from './landing/LandingIntro';
 export { default as MaterialCard } from './cards/MaterialCard';
+export { default as PersonData } from './Profile/PersonData';
+export { default as ProjectBox } from './Profile/ProjectBox';
