06782b64cefbdd871695b45b4b77214f946df336
Adrian <adrianfdez469@gmail.com>
2021-02-15 19:44:50 -0500

    Developing integration with Gtihub

--- components/UI/ProjectEditor/styles.js
+++ components/UI/ProjectEditor/styles.js
@@ -120,3 +120,38 @@ export const useGalleryStyles = makeStyles((theme) => ({
     },
   },
 }));
+
+export const useSyncStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: theme.spacing(32),
+  },
+  smallAvatar: {
+    width: theme.spacing(4),
+    height: theme.spacing(4),
+    marginRight: theme.spacing(1),
+  },
+  flexCenter: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  progress: {
+    width: '100%',
+    // height: theme.spacing(0.),
+    // borderRadius: theme.spacing(0.2),
+    position: 'absolute',
+    bottom: 0,
+  },
+  formcontrolWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+    [theme.breakpoints.up(800)]: {
+      marginLeft: theme.spacing(5),
+      marginRight: theme.spacing(5),
+    },
+  },
+}));
