b4afe6289b678a023a188e0957afe2c50b1521d2
Jorge Arce <jarce@xetid.cu>
2021-02-19 22:10:24 +0100

    Actualización del mecanismo para cambio de idioma. Y creación de mecanimos de manejo de estado con Recoil

--- views/profile/Profile.js
+++ views/profile/Profile.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { PersonData, ProjectBox } from '../../components/UI';
 
-const Profile = () => (
-  <>
-    <PersonData />
-    <ProjectBox />
-  </>
-);
+const Profile = (props) => {
+  const { edit } = props;
+  return (
+    <>
+      <PersonData edit={edit} />
+      <ProjectBox />
+    </>
+  );
+};
 
 export default Profile;
