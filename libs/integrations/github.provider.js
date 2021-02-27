80582704a7639f51bc8bf5468687de13335e91a7
Adrian <adrianfdez469@gmail.com>
2021-02-26 21:18:51 -0500

    Organizing github.provider

--- libs/integrations/github.provider.js
+++ libs/integrations/github.provider.js
@@ -1,7 +1,7 @@
 import { getSession } from 'next-auth/client';
 import prisma from '../../prisma/prisma.instance';
 
-const getReposQuery = (first = 50) => `
+const gqlRepos = (first = 50) => `
   query { 
     viewer { 
       repositories(first:${Math.max(first, 50)}){
@@ -15,43 +15,113 @@ const getReposQuery = (first = 50) => `
             login
             avatarUrl
           }
+          isPrivate
         }
       }
     }
   }`;
 
-export const getGithubRepos = async (context) => {
+const gqlRepoData = (projectId) => `
+  query { 
+    node(id:"${projectId}"){
+      __typename
+      ... on Repository {
+        id
+        name
+        description
+        createdAt
+        url
+        deployments(last:1) {
+          nodes {
+            latestStatus {
+              environmentUrl
+            }
+          }
+        }
+        languages(first: 50){
+          nodes{
+            name
+          }
+        }
+        mentionableUsers (first: 6) {
+          nodes {
+            login
+            avatarUrl
+            email
+            bio
+            name
+            url
+          }
+          totalCount
+        }
+        owner {
+          id
+          login
+        }
+      }
+    }
+  }`;
+
+const getGithubEnhanceToken = async (userId) => {
+  const userToken = await prisma.userTokens.findUnique({
+    where: {
+      userId_provider: {
+        provider: 'github',
+        userId,
+      },
+    },
+  });
+  if (userToken) return userToken.accessToken;
+  return null;
+};
+
+const getGithubToken = async (context) => {
   const session = await getSession(context);
+
   if (!session) {
     throw new Error('NO_SESSION');
   }
-  if (session.tokenProvider !== 'github') {
-    const userToken = await prisma.userTokens.findUnique({
-      where: {
-        userId_provider: {
-          provider: 'github',
-          userId: session.userId,
-        },
-      },
-    });
-    if (!userToken) {
+  const accessToken = await getGithubEnhanceToken(session.userId);
+  if (!accessToken) {
+    if (session.tokenProvider !== 'github') {
       throw new Error('NO_GITHUB_TOKEN');
-    } else {
-      session.accessToken = userToken.accessToken;
     }
+    if (!session.accessToken) {
+      throw new Error('NO_ACCESS_TOKEN_ON_SESSION');
+    }
+    return session.accessToken;
   }
-  if (!session.accessToken) {
-    throw new Error('NO_ACCESS_TOKEN_ON_SESSION');
-  }
+  return accessToken;
+};
 
+const getGithubEnhaceToken = async (code, state) => {
+  const response = await fetch(
+    `https://github.com/login/oauth/access_token?client_id=${process.env.NEXT_PUBLIC_GITHUB_ID}&client_secret=${process.env.GITHUB_SECRET}&code=${code}&state=${state}`,
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error('GET_TOKEN_FAIL');
+  }
+  const accessToken = await response.json();
+  return accessToken.access_token;
+};
+
+export const getGithubRepos = async (context) => {
+  const accessToken = await getGithubToken(context);
   const response = await fetch('https://api.github.com/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${session.accessToken}`,
+      Authorization: `Bearer ${accessToken}`,
     },
     body: JSON.stringify({
-      query: getReposQuery(),
+      query: gqlRepos(),
     }),
   });
   if (response.ok) {
@@ -66,80 +136,17 @@ export const getGithubRepos = async (context) => {
   throw new Error('INTERNAL_ERROR');
 };
 
-const getRepoData = (projectId) => `
-query { 
-  node(id:"${projectId}"){
-    __typename
-    ... on Repository {
-      id
-      name
-      description
-      createdAt
-      url
-      deployments(last:1) {
-        nodes {
-          latestStatus {
-            environmentUrl
-          }
-        }
-      }
-      languages(first: 50){
-        nodes{
-          name
-        }
-      }
-      mentionableUsers (first: 6) {
-        nodes {
-          login
-          avatarUrl
-          email
-          bio
-          name
-          url
-        }
-        totalCount
-      }
-      owner {
-        id
-        login
-      }
-    }
-  }
-}`;
-
 export const getGithubRepoData = async (context, projectId) => {
-  const session = await getSession(context);
-
-  if (!session) {
-    throw new Error('NO_SESSION');
-  }
-  if (session.tokenProvider !== 'github') {
-    const userToken = await prisma.userTokens.findUnique({
-      where: {
-        userId_provider: {
-          provider: 'github',
-          userId: session.userId,
-        },
-      },
-    });
-    if (!userToken) {
-      throw new Error('NO_GITHUB_TOKEN');
-    } else {
-      session.accessToken = userToken.accessToken;
-    }
-  }
-  if (!session.accessToken) {
-    throw new Error('NO_ACCESS_TOKEN_ON_SESSION');
-  }
+  const accessToken = await getGithubToken(context);
 
   const response = await fetch('https://api.github.com/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${session.accessToken}`,
+      Authorization: `Bearer ${accessToken}`,
     },
     body: JSON.stringify({
-      query: getRepoData(projectId),
+      query: gqlRepoData(projectId),
     }),
   });
 
@@ -151,4 +158,36 @@ export const getGithubRepoData = async (context, projectId) => {
     };
   }
   throw new Error('CANT_LOAD_PROVIDER_DATA');
+};
+
+export const saveGithubEnhanceToken = async (userId, code, state) => {
+  const accessToken = await getGithubEnhaceToken(code, state);
+
+  await prisma.userTokens.upsert({
+    where: {
+      userId_provider: {
+        provider: state,
+        userId,
+      },
+    },
+    create: {
+      accessToken,
+      provider: state,
+      userId,
+    },
+    update: {
+      accessToken,
+    },
+  });
+};
+
+export const deleteGithubEnhanceToken = async (userId) => {
+  prisma.userTokens.delete({
+    where: {
+      userId_provider: {
+        provider: 'github',
+        userId,
+      },
+    },
+  });
 };
--- pages/api/customauth.js
+++ pages/api/customauth.js
@@ -1,5 +1,5 @@
 import { getSession } from 'next-auth/client';
-import prisma from '../../prisma/prisma.instance';
+import { saveGithubEnhanceToken } from '../../libs/integrations/github.provider';
 
 const handler = async (req, res) => {
   const { code, state } = req.query;
@@ -7,39 +7,11 @@ const handler = async (req, res) => {
 
   if (state === 'github') {
     // TODO: hacer esto en el github.provider
+    saveGithubEnhanceToken(session.userId, code, state);
 
-    const response = await fetch(
-      `https://github.com/login/oauth/access_token?client_id=${process.env.NEXT_PUBLIC_GITHUB_ID}&client_secret=${process.env.GITHUB_SECRET}&code=${code}&state=${state}`,
-      {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-        },
-      }
-    );
-
-    if (!response.ok) {
-      // Error
-    }
-    const accessToken = await response.json();
-    await prisma.userTokens.upsert({
-      where: {
-        userId_provider: {
-          provider: state,
-          userId: session.userId,
-        },
-      },
-      create: {
-        accessToken: accessToken.access_token,
-        provider: state,
-        userId: session.userId,
-      },
-      update: {
-        accessToken: accessToken.access_token,
-      },
+    res.writeHead(301, {
+      Location: `${process.env.NEXTAUTH_URL}/profile/${session.userId}/newproject`,
     });
-
-    res.writeHead(301, { Location: 'http://localhost:3000/profile/1/newproject' });
     res.end();
   }
   if (state === 'gitlab') {
