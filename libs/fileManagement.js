087925d9129d44af4884eb9ba48ddc88ec749ffb
Adrian <adrianfdez469@gmail.com>
2021-04-26 13:10:21 -0500

    [Fix] Switching firebase for Cloudinary for as images cloud provider

--- libs/fileManagement.js
+++ libs/fileManagement.js
@@ -1,69 +1,30 @@
-import { generateHash, generateUuid } from './generators';
-import firebaseAdmin from './integrations/firebaseAdmin';
+import cloudinary from './integrations/cloudinary';
 
 const fs = require('fs');
 
 const getLocalFileUrl = (image) => `${image.destination}/${image.originalname}`;
 
-// set the Firebase metadata with the downloadToken
-const setFirebaseMetadata = async (fileRef) => {
-  const downloadToken = generateUuid();
-  const metadata = {
-    metadata: {
-      firebaseStorageDownloadTokens: downloadToken,
-    },
-  };
-
-  await fileRef.setMetadata(metadata);
-
-  return downloadToken;
-}; // setFirebaseMetadata
-
-// build the downloadURL using the file infomation; adds the download token if it doesn't exists
-const getDownloadURL = async (fileRef) => {
-  const bucket = fileRef.bucket.name;
-  const [metadata] = await fileRef.getMetadata();
-  let downloadToken;
-
-  if (!metadata.metadata) {
-    downloadToken = await setFirebaseMetadata(fileRef);
-  } else {
-    [downloadToken] = metadata.metadata.firebaseStorageDownloadTokens.split(',');
-  }
-
-  const url = `https://firebasestorage.googleapis.com/v0/b/${bucket}/o/${encodeURIComponent(
-    fileRef.name
-  )}?alt=media&token=${downloadToken}`;
-
-  return url;
-}; // getDownloadURL
-
-const uploadFileToCloud = async (image) => {
-  const imageTempPath = getLocalFileUrl(image);
-  const bucket = firebaseAdmin.storage().bucket();
-  const url = await new Promise((resolve, reject) => {
-    bucket.upload(
-      imageTempPath,
-      {
-        destination: `${generateHash()}-${image.originalname}`,
-        gzip: false,
-      },
-      (err, file) => {
-        if (!err) {
-          getDownloadURL(file).then((fileUrl) => {
-            resolve(fileUrl);
-          });
-        } else {
-          reject(err);
+const uploadFileToCloudinary = async (image) => {
+  try {
+    const imageTempPath = getLocalFileUrl(image);
+    const url = await new Promise((resolve, reject) => {
+      cloudinary.uploader.upload(imageTempPath, (error, result) => {
+        if (result) {
+          resolve(result);
         }
-      }
-    );
-  }).then((imageUrl) => {
-    fs.promises.unlink(imageTempPath);
-    return imageUrl;
-  });
-
-  return url;
+        if (error) {
+          reject(error);
+        }
+      });
+    }).then((result) => {
+      fs.promises.unlink(imageTempPath);
+      return result.url;
+    });
+    return url;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 };
 
 export const saveFile = async (file) => {
@@ -72,7 +33,8 @@ export const saveFile = async (file) => {
     const imgUrl = `${process.env.NEXTAUTH_URL}${url.split('./public')[1]}`;
     return imgUrl;
   }
-  const imageUrl = await uploadFileToCloud(file);
+
+  const imageUrl = await uploadFileToCloudinary(file);
   return imageUrl;
 };
 
@@ -82,18 +44,27 @@ const deleteLocalFile = async (path) => {
   return fs.promises.unlink(`./public/uploads/${name}`).then(() => true);
 };
 
-const deleteFileFromCluod = async (path) => {
-  const bucket = firebaseAdmin.storage().bucket();
-  const filePath = path
-    .split(`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/`)[1]
-    .split('?')[0];
-  const file = bucket.file(filePath);
-  return file.delete().then(() => true);
+const deleteFileFromCloudinary = async (path) => {
+  try {
+    const publicId = path.split('/').pop().split('.')[0];
+    const res = await new Promise((resolve, reject) => {
+      cloudinary.uploader.destroy(publicId, (error, result) => {
+        if (error) reject(error);
+        if (result) resolve(result);
+      });
+    });
+
+    if (res.result !== 'ok') throw new Error('ERROR_DELETING_IMG');
+    return true;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 };
 
 export const deleteFile = async (path) => {
   if (process.env.NODE_ENV === 'development') {
     return deleteLocalFile(path);
   }
-  return deleteFileFromCluod(path);
+  return deleteFileFromCloudinary(path);
 };
