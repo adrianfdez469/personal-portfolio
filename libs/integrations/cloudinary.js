6bbfb4fa8ad3f849ef6efc3b985aadcb7d442754
Adrian <adrianfdez469@gmail.com>
2021-05-02 01:38:15 -0500

    Adding delete images on cloudinary

--- libs/integrations/cloudinary.js
+++ libs/integrations/cloudinary.js
@@ -6,4 +6,12 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+export const deleteImagesPromise = (images) =>
+  new Promise((resolve, reject) => {
+    cloudinary.v2.api.delete_resources(images, (err, result) => {
+      if (err) reject(err);
+      if (result) resolve(result);
+    });
+  });
+
 export default cloudinary.v2;
