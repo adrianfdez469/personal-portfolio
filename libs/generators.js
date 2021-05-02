08a4f5562d29743d5ddcb7895bc9d0fe297d91f8
Adrian <adrianfdez469@gmail.com>
2021-05-02 01:38:47 -0500

    Little fix of getSlug function

--- libs/generators.js
+++ libs/generators.js
@@ -4,7 +4,7 @@ import createUuid from 'uuid-v4';
 export const generateHash = () =>
   Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 
-export const getSlug = (text) => limax(text);
+export const getSlug = (text) => limax(text || '');
 
 export const getHashSlug = (text) => `${text ? `${getSlug(text)}-` : ''}${generateHash()}`;
 
